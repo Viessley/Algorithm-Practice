8bb8fa594fb64a2ba7a5f11921e4fee14ec59255
Wesley <galileowes@gmail.com>
2025-11-29 23:19:39 -0500

    Solved 350. Intersection of Two Arrays II

--- 350.intersection-of-two-arrays-ii.java
+++ 350.intersection-of-two-arrays-ii.java
@@ -5,9 +5,29 @@
  */
 
 // @lc code=start
+Import java.util.map;
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
+        Map<Integer, Integer> map = new HashMap<>();        
+            for(int e : nums1){
+                map.put(e, map.getOrDefault(e, 0) + 1);
+            }
+
+        List<Integer> outcome = new ArrayList<>();
+            for(int e : nums2){
+                if(map.containsKey(e) && map.get(e) > 0){
+                    outcome.add(e);
+                    map.put(e, map.get(e) - 1);
+                }
+            }
+            
+        int[] result = new int[outcome.size()];
+            for(int i = 0; i < outcome.size(); i++){
+                result[i] = outcome.get(i);
+            }
+
+        return result;
     }
 }
 // @lc code=end
